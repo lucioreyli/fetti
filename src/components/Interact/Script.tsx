07504654ce4cfffe810105db5a98948d938833b3
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-16 02:41:22 -0400

    refactor: removing unused function

--- src/components/Interact/Script.tsx
+++ src/components/Interact/Script.tsx
@@ -38,37 +38,16 @@ export const Script: FC = () => {
 
   const preventTab: KeyboardEventHandler<HTMLDivElement> = (e) => {
     if (e.key !== 'Tab' || !inputRef.current) return;
-    // document.execCommand('insertHTML', false, '&#009');
-    function insertTab() {
-      if (!window.getSelection) return;
-      const sel = window.getSelection();
-      if (!sel?.rangeCount) return;
-      const range = sel.getRangeAt(0);
-      range.collapse(true);
-      const span = document.createElement('span');
-      span.appendChild(document.createTextNode('\t'));
-      span.style.whiteSpace = 'pre';
-      range.insertNode(span);
-      // Move the caret immediately after the inserted span
-      range.setStartAfter(span);
-      range.collapse(true);
-      sel.removeAllRanges();
-      sel.addRange(range);
-    }
-    const fn1 = () => {
-      const selection =
-        inputRef.current!.ownerDocument.defaultView?.getSelection();
-      if (!selection || selection?.rangeCount === 0) return;
-      const range = selection.getRangeAt(0);
-      const tabNode = document.createTextNode('\u00a0\u00a0\u00a0\u00a0');
-      range.insertNode(tabNode);
-      range.setStartAfter(tabNode);
-      range.setEndAfter(tabNode);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    };
-    fn1();
-
+    const selection =
+      inputRef.current.ownerDocument.defaultView?.getSelection();
+    if (!selection || selection?.rangeCount === 0) return;
+    const range = selection.getRangeAt(0);
+    const tabNode = document.createTextNode('\u00a0\u00a0\u00a0\u00a0');
+    range.insertNode(tabNode);
+    range.setStartAfter(tabNode);
+    range.setEndAfter(tabNode);
+    selection.removeAllRanges();
+    selection.addRange(range);
     e.preventDefault();
   };
 
