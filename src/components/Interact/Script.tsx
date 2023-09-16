5e7627a0853c0838f869abf7f0c07c897b5726c6
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-16 02:40:40 -0400

    feat: add tab on input

--- src/components/Interact/Script.tsx
+++ src/components/Interact/Script.tsx
@@ -1,4 +1,5 @@
 'use client';
+import parseHtml from 'html-react-parser';
 import {
   type FC,
   useState,
@@ -14,10 +15,9 @@ hljs.registerLanguage('pgsql', pgsql);
 
 export const Script: FC = () => {
   const [text, setText] = useState(
-    hljs.highlight('-- Insert here your script', {
-      language: 'pgsql',
-    }).value,
+    hljs.highlight('-- Insert here your script', { language: 'pgsql' }).value,
   );
+  const inputRef = useRef<null | HTMLDivElement>(null);
 
   const timeoutId = useRef<NodeJS.Timeout | number | null>(null);
 
@@ -37,12 +37,8 @@ export const Script: FC = () => {
   };
 
   const preventTab: KeyboardEventHandler<HTMLDivElement> = (e) => {
-    if (e.key !== 'Tab') {
-      return;
-    }
-    e.preventDefault();
-
-    //   document.execCommand('insertHTML', false, '&#009');
+    if (e.key !== 'Tab' || !inputRef.current) return;
+    // document.execCommand('insertHTML', false, '&#009');
     function insertTab() {
       if (!window.getSelection) return;
       const sel = window.getSelection();
@@ -59,19 +55,30 @@ export const Script: FC = () => {
       sel.removeAllRanges();
       sel.addRange(range);
     }
+    const fn1 = () => {
+      const selection =
+        inputRef.current!.ownerDocument.defaultView?.getSelection();
+      if (!selection || selection?.rangeCount === 0) return;
+      const range = selection.getRangeAt(0);
+      const tabNode = document.createTextNode('\u00a0\u00a0\u00a0\u00a0');
+      range.insertNode(tabNode);
+      range.setStartAfter(tabNode);
+      range.setEndAfter(tabNode);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    };
+    fn1();
 
-    const start = e.selectionStart;
-    const end = e.selectionEnd;
-    setText(text.slice(0, start) + '\t' + text.slice(end));
-    e.target.setSelectionRange(start + indentSize, start + indentSize);
+    e.preventDefault();
   };
 
   return (
     <pre className="flex-1 flex w-full">
       <div
+        ref={inputRef}
         id="text"
-        className="code language-pgsql leading-relaxed rounded-md flex-1 p-4 !bg-muted border border-input px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 w-full whitespace-normal"
-        onInput={ab}
+        className="leading-relaxed rounded-md flex-1 p-4 !bg-muted border border-input px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 w-full whitespace-normal"
+        //onInput={ab}
         onKeyDown={preventTab}
         contentEditable={'plaintext-only' as unknown as boolean}
         autoCorrect="off"
@@ -79,7 +86,7 @@ export const Script: FC = () => {
         autoCapitalize="off"
         aria-autocomplete="none"
         dangerouslySetInnerHTML={{ __html: text }}
-      />
+      ></div>
     </pre>
   );
   // return <Textarea className="h-full flex-1 leading-relaxed resize-none" />;
