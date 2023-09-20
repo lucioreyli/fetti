ba41ccdee3110d33cb8d3e57996b88654903b6b0
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-20 09:05:01 -0400

    feat: removing useEffect and useState

--- src/components/Interact/Actions.tsx
+++ src/components/Interact/Actions.tsx
@@ -1,13 +1,30 @@
 'use client';
 import { useCodeStore } from '@/store/code-store';
 import { Button } from '../ui/button';
+import { useEffect } from 'react';
+import { useMonaco } from '@monaco-editor/react';
 
 export const Actions = () => {
   const code = useCodeStore((state) => state.code);
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    const hi = (e: KeyboardEvent) => {
+      console.log(e);
+      if (!e.isTrusted || !e.metaKey) return;
+      if (e.code === 'Enter') {
+        if (!e.shiftKey) return console.log('metebala');
+      }
+      e.preventDefault();
+    };
+    document.onkeydown = hi;
+    if (monaco) {
+    }
+  }, [code, monaco]);
 
   return (
-    <div className="ml-auto">
-      {code}
+    <div className="ml-auto flex gap-2">
+      <Button variant="secondary">Run selection</Button>
       <Button>Run</Button>
     </div>
   );
--- src/components/Interact/Script.tsx
+++ src/components/Interact/Script.tsx
@@ -1,46 +1,37 @@
 'use client';
-import Editor, { useMonaco } from '@monaco-editor/react';
+import Editor, { type Monaco } from '@monaco-editor/react';
 import { useTheme } from 'next-themes';
-import { useEffect, type FC, useState } from 'react';
+import type { FC } from 'react';
 import { editorConfig } from './editor-config';
 import { useCodeStore } from '@/store/code-store';
 
 export const Script: FC = () => {
-  const monaco = useMonaco();
   const setCode = useCodeStore((state) => state.setCode);
   const { resolvedTheme } = useTheme();
-  const [themeLoaded, setThemeLoaded] = useState(false);
 
-  useEffect(() => {
-    if (!monaco) return;
-    const loadTheme = () => {
-      const darkTheme = require('./github-dark.json');
-      const lightTheme = require('./github-light.json');
-      monaco.editor.defineTheme('github-light', lightTheme);
-      monaco.editor.defineTheme('github-dark', darkTheme);
-      setThemeLoaded(true);
-    };
-    loadTheme();
-  }, [resolvedTheme, monaco]);
+  const loadTheme = (monaco: Monaco) => {
+    const darkTheme = require('./github-dark.json');
+    const lightTheme = require('./github-light.json');
+    monaco.editor.defineTheme('github-light', lightTheme);
+    monaco.editor.defineTheme('github-dark', darkTheme);
+  };
 
   const currentTheme =
     resolvedTheme === 'light' ? 'github-light' : 'github-dark';
   const onChange = (code: string | undefined) => setCode(code ?? '');
+
   // TODO: fix code editor width resizing
   return (
     <div className="border bg-muted flex-1 flex rounded overflow-hidden text-xl min-w-0 min-h-0">
-      {themeLoaded ? (
-        <Editor
-          className="font-mono rounded flex-1 min-w-0 min-h-0 w-auto"
-          options={editorConfig}
-          language="pgsql"
-          defaultValue="// some comment"
-          theme={currentTheme}
-          onChange={onChange}
-        />
-      ) : (
-        <></>
-      )}
+      <Editor
+        className="font-mono rounded flex-1 min-w-0 min-h-0 w-auto"
+        options={editorConfig}
+        language="sql"
+        defaultValue="// some comment"
+        theme={currentTheme}
+        beforeMount={loadTheme}
+        onChange={onChange}
+      />
     </div>
   );
 };
