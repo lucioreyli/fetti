66deee8d8641b4b84818ee4272d238400141b2d1
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-22 12:08:53 -0400

    fix: next config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
   output: 'export',
   transpilePackages: ['@tauri-apps/api'],
-  entry: {
-    'sparksql.worker':
-      'monaco-sql-languages/out/esm/sparksql/sparksql.worker.js',
-    'flinksql.worker':
-      'monaco-sql-languages/out/esm/flinksql/flinksql.worker.js',
-    'hivesql.worker': 'monaco-sql-languages/out/esm/hivesql/hivesql.worker.js',
-    'mysql.worker': 'monaco-sql-languages/out/esm/mysql/mysql.worker.js',
-    'pgsql.worker': 'monaco-sql-languages/out/esm/pgsql/pgsql.worker.js',
-    'plsql.worker': 'monaco-sql-languages/out/esm/plsql/plsql.worker.js',
-    'sql.worker': 'monaco-sql-languages/out/esm/sql/sql.worker.js',
-  },
 };
 
 module.exports = nextConfig;
